179cd9f5ab15902ac0808db15a1b9201fe6869de
opeyemi olaitan <johnolaitan22@gmail.com>
2026-08-14 13:51:03 +0100

    cors fix

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://prospect-tracker-tawny.vercel.app",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
